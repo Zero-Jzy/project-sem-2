e2da29eedb14d4c732a9e4e7641616d77a7f0cfa
Zero-Jzy <dekujzy@gmail.com>
2019-08-02 07:57:04 +0700

    commit ne

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Foundation\Bus\DispatchesJobs;
+use Illuminate\Http\Request;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
@@ -10,4 +11,14 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    public function AuthApi(Request $request)
+    {
+        return $request->user();
+    }
+
+    public function index()
+    {
+        return view('welcome');
+    }
 }
