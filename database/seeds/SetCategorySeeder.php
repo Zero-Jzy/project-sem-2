f22e691badb715fb75b76d90f1d854b0a8d1d56a
Anh Hoc Dot <anh@hocdot.com>
2019-09-05 20:56:07 +0700

    DishSeeder

--- database/seeds/SetCategorySeeder.php
+++ database/seeds/SetCategorySeeder.php
@@ -15,8 +15,11 @@ class SetCategorySeeder extends Seeder
         DB::table('set_categories')->truncate();
         DB::table('set_categories')->insert([
 
-            ['id' => 1, 'name' => 'demo', 'description' => 'demo']
-
+            ['id' => 1, 'name' => 'ration for fat people', 'description' => 'Meals will help you provide the necessary nutrients and lose a lot of weight when you apply. 
+            Providing enough calories does not make you feel anxious or too hungry in each meal, 
+            to avoid the situation. indigestion or appetite, and does not improve the desired weight'],
+            ['id' => 2, 'name' => 'ration for thin people', 'description' => 'Meals will help you provide the necessary nutrients. Increase weight to match your weight height'],
+            ['id' => 3, 'name' => 'ration for office workers', 'description' => 'ration provides all the necessary nutrients for a stressful, tired working day'],
         ]);
     }
 }
