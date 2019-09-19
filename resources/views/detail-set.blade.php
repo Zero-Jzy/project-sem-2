82c56f94ef45924d73ddf23f4a7fd13c31c50e90
Anh Hoc Dot <anh@hocdot.com>
2019-09-19 11:18:46 +0700

    sửa tràn giao dien trang food

---
+++ resources/views/detail-set.blade.php
@@ -0,0 +1,19 @@
+@extends('layouts.master', ['title' => 'Detail set'])
+@section('content')
+
+    <div class="container" style="margin-top: 150px">
+        <div class="row ">
+            <div class="col-5">
+                <div class="about_img">
+                    <img src="https://www.petmd.com/sites/default/files/Acute-Dog-Diarrhea-47066074.jpg" alt="">
+                </div>
+            </div>
+            <div class="col-7">
+                <h4 class="name_title" style="margin-left: 100px">suon xào chua ngọt</h4>
+{{--                <h3>Name</h3>--}}
+{{--                <h3>Name</h3>--}}
+            </div>
+            <div class="col">Column</div>
+        </div>
+    </div>
+@endsection
